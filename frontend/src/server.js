306e779c99af4b79ff62c09cf436b7a639928498
Tanmay Haldar <iamtanmay2959@gmail.com>
2025-04-25 19:02:42 +0530

    ..

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,5 +1,5 @@
-//  export const server = "https://classiccustom-backend.onrender.com/api/v2";
-//  export const imgServer = "https://classiccustom-backend.onrender.com/uploads/"
+ export const server = "https://classiccustom-backend.onrender.com/api/v2";
+ export const imgServer = "https://classiccustom-backend.onrender.com/uploads/"
 
-export const server = "http://localhost:5000/api/v2";
-export const imgServer = "http://localhost:5000/uploads/" 
+// export const server = "http://localhost:5000/api/v2";
+// export const imgServer = "http://localhost:5000/uploads/" 
